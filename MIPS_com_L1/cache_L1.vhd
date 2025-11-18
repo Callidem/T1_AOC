9d0c633395de523a39e23d669c903b29dda1ca8e
Callidem <brunomonti00@gmail.com>
2025-11-18 20:29:08 -0300

    corrigidos nomes de constantes para maiúsculo, adicionadas algumas portas e modificadas outras

--- MIPS_com_L1/cache_L1.vhd
+++ MIPS_com_L1/cache_L1.vhd
@@ -25,31 +25,35 @@ use IEEE.Std_Logic_1164.all;
 use IEEE.Std_Logic_unsigned.all;
 
 entity cache is
-      generic(word: integer := 32; 
-              block_size: integer := 8;
-              num_lines: integer := 8;
-              line_size: integer := 1+26+32*8-1
+      generic(WORD: integer := 32; 
+              BLOCK_SIZE: integer := 8;
+              NUM_LINES: integer := 8;
+              LINE_SIZE: integer := 1+26+32*8-1
              );
-      -- calculo do tamanho da linha (bit_de_validade + tag + tamanho_da_palavra*block_size - 1)
+      -- calculo do tamanho da linha (bit_de_validade + tag + tamanho_da_palavra*BLOCK_SIZE - 1)
 
       port( data_io:   inout std_logic_vector(31 downto 0); -- dado a ser ecrito ou a ser enviado
             addr_i:    in    std_logic_vector(31 downto 0); -- endereco do dado
             ce_i:      in    std_logic; -- chip enable
             we_i:      in    std_logic; -- write enable
             oe_i:      in    std_logic; -- read enable
-            hold:      in    std_logic;
+            hold_i:    in    std_logic;
             clk:       in    std_logic;
             rst:       in    std_logic;
             -- fio para comunicacao com a proxima memoria
+            hold_o:      out   std_logic;
             addr_o:    out   std_logic_vector(31 downto 0); -- endereco do dado
-            ce_o:      out   std_logic -- chip enable proxima memoria
+            ce_o:      out   std_logic; -- chip enable proxima memoria
+            oe_o:      out   std_logic; -- read enable proxima memoria
+            we_o:      out   std_logic  -- write enable proxima memoria
+
             ); -- 1 out esta pronto, 0 em operacao
 end;
 
 architecture cache of cache is
 
   type cache_state is (idle, read, write, cache_miss);
-  type cache_memory is array(0 to num_lines) of std_logic_vector(line_size downto 0);
+  type cache_memory is array(0 to NUM_LINES-1) of std_logic_vector(LINE_SIZE-1 downto 0);
 
   signal memory:  cache_memory;
   signal state:   cache_state;
@@ -75,8 +79,8 @@ begin
       state    <= idle;
       aux      <=  0;
 
-      for i in 0 to num_lines loop
-            memory(i)(line_size) <= '0'; -- zera bit de validade
+      for i in 0 to NUM_LINES loop
+            memory(i)(LINE_SIZE) <= '0'; -- zera bit de validade
       end loop;
 
       -- falta coisa ainda
@@ -96,7 +100,7 @@ begin
 
           when read =>
 
-            if (tag /= memory(linha)(31 downto 6)) and (memory(linha)(line_size) = '1') then -- compara tag, e se memoria for valida, caso cache miss
+            if (tag /= memory(linha)(31 downto 6)) and (memory(linha)(LINE_SIZE) = '1') then -- compara tag, e se memoria for valida, caso cache miss
               state <= cache_miss;
             else
               data_io <= memory(linha)((bloco + 1)*32 -1 downto bloco * 32);
@@ -104,7 +108,7 @@ begin
 
           when write =>
 
-            if (tag /= memory(linha)(31 downto 6)) and (memory(linha)(line_size) = '1') then -- compara tag, e se memoria for valida, caso cache miss
+            if (tag /= memory(linha)(31 downto 6)) and (memory(linha)(LINE_SIZE) = '1') then -- compara tag, e se memoria for valida, caso cache miss
               state <= cache_miss;
             end if;
 
